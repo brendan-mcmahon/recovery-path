57d49ec00a64d4ac92a2ace17b7fa09a6a5f64bd
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2023-07-17 14:33:26 -0400

    attempting to optimize

---
+++ docs/main.js
@@ -0,0 +1,38 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
+function calculateArea(index) {
+    var length = document.getElementById(`length${index}`).value;
+    var width = document.getElementById(`width${index}`).value;
+
+    var area = length * width;
+    document.getElementById(`area${index}`).innerHTML =
+      length && width ? +area.toFixed(2) + " cm&sup2;" : "- cm&sup2;";
+    return area;
+  }
+
+window.calculate = function() {
+    var area1 = calculateArea(1);
+    var area2 = calculateArea(2);
+
+    if (area1 && area2) {
+      var difference = Math.abs(area1 - area2);
+      var percentageDifference = (Math.abs(area2 - area1) / ((area1 + area2) / 2)) * 100;
+      var percentageChange = ((area2 - area1) / area1) * 100;
+
+      document.getElementById("diff").innerHTML = +difference.toFixed(2) + " cm&sup2;";
+      document.getElementById("diff-percent").innerHTML = +percentageDifference.toFixed(2) + "%";
+      document.getElementById("change").innerHTML = +percentageChange.toFixed(2) + "%";
+    } else {
+      document.getElementById("diff").innerHTML = "- cm&sup2;";
+      document.getElementById("diff-percent").innerHTML = "- %";
+      document.getElementById("change").innerHTML = "- %";
+    }
+  }
---
+++ service-worker.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = 'recovery-path-v1';
+var urlsToCache = [
+  './docs/index.html',
+  './docs/index.css',
+  './docs/main.js',
+  './docs/icon.svg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
